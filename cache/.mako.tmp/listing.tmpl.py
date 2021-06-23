2ab423b1ac06bad9e25af0e7023c50dcdbe7a9d1
Marco Hassan <marco.hassan30@gmail.com>
2021-06-23 19:43:03 +0200

    Nikola auto commit.
    Nikola version: 8.1.2

--- cache/.mako.tmp/listing.tmpl.py
+++ cache/.mako.tmp/listing.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1623962963.144205
+_modified_time = 1624470178.362127
 _enable_loop = True
 _template_filename = '/Users/marcohassan/Desktop/python_venv/nikola/lib/python3.9/site-packages/nikola/data/themes/base/templates/listing.tmpl'
 _template_uri = 'listing.tmpl'
@@ -30,15 +30,15 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        code = context.get('code', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
         crumbs = context.get('crumbs', UNDEFINED)
+        files = context.get('files', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
-        messages = context.get('messages', UNDEFINED)
-        files = context.get('files', UNDEFINED)
-        source_link = context.get('source_link', UNDEFINED)
         title = context.get('title', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
+        source_link = context.get('source_link', UNDEFINED)
+        code = context.get('code', UNDEFINED)
+        messages = context.get('messages', UNDEFINED)
         folders = context.get('folders', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
@@ -56,15 +56,15 @@ def render_body(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        code = context.get('code', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
         crumbs = context.get('crumbs', UNDEFINED)
+        files = context.get('files', UNDEFINED)
         def content():
             return render_content(context)
-        messages = context.get('messages', UNDEFINED)
-        files = context.get('files', UNDEFINED)
-        source_link = context.get('source_link', UNDEFINED)
         title = context.get('title', UNDEFINED)
+        ui = _mako_get_namespace(context, 'ui')
+        source_link = context.get('source_link', UNDEFINED)
+        code = context.get('code', UNDEFINED)
+        messages = context.get('messages', UNDEFINED)
         folders = context.get('folders', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
